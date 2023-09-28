2fdab29d7a96591544a8f8a17be47b55c0ad13f5
Neoyacine <neoyacine13@gmail.com>
2023-09-28 12:49:29 +0200

    28/09

--- compteur.js
+++ compteur.js
@@ -1,14 +1,13 @@
 // Compteur et bouton
 let count = 0;
-// FONCTION POUR AJOUTER UN (BOUTON)
 function addOne(id) {
     let counter = document.querySelector("#compteur" + id);
     counter.innerText++;
-
     update();
 }
+
 // METTRE CONDITION < 0
-// FONCTION POUR SOUSTRAIRE DE UN (BOUTON)
+
 function minusOne(id) {
     let counter = document.querySelector("#compteur" + id);
     if (counter.innerText > 0) {
@@ -17,56 +16,49 @@ function minusOne(id) {
     else {
         counter.innerText = 0;
     }
-
     update();
 }
 
-
 // PRIX ET QUANTITE
 let numbers = document.querySelectorAll(".totCompteur")
-
 function update() {
-
-    let quantite = [
-    ];
-
+    let quantite = [];
     let i = 0;
 
     // ACTUALISER POUR LES NOMBRES < 0 avec if etc .
 
     numbers.forEach(number => {
-        // FILTRER, récupérer entier positif
         quantite.push([number.innerText, i])
         i++
     });
 
 
-   
-
-    // TOTAL (croisement des deux tableaux)
     let j = 0;
     let prixTotal = 0;
     let quantiteTotal = 0;
-
     quantite.forEach(number => {
-
         ticketList.forEach(ticket => {
-            // On compare les deux id
-            // number[1] car l'id est le deuxième champ
             if (ticket.id === number[1]) {
                 prixTotal += ticket.prix / 100 * parseInt(number[0], 10);
-                // ARRONDIR LE TOTAL A 1 virgule
                 j++
             }
         });
         quantiteTotal += parseInt(number[0], 10);
     });
 
-  
-    // console.log(prixTotal)
-    // console.log(quantiteTotal);
+    // Créez un objet pour stocker la quantité achetée pour chaque ticket
+    let quantitesParTicket = {};
+    quantite.forEach(number => {
+        ticketList.forEach(ticket => {
+            // On compare les deux id
+            if (ticket.id === number[1]) {
+                if (quantitesParTicket[ticket.id]) {
+                    quantitesParTicket[ticket.id] = parseInt(number[0], 10)
+                }
 
-
+            }
+        });
+    });
 
     let panier = document.querySelector(".panier")
     if (prixTotal > 0) {
@@ -78,6 +70,10 @@ function update() {
 
     let montant = document.querySelector(".montant")
     montant.innerText = prixTotal + " €"
+    let montantP = document.querySelector(".montantPaiement")
+    montantP.innerText = prixTotal + " €"
+    let montantCB = document.querySelector(".montantCB")
+    montantCB.innerText = prixTotal + " €"
 
     let panierQuantite = document.querySelector(".quantite")
     panierQuantite.innerText = "(" + quantiteTotal + ")"
--- paiement.js
+++ paiement.js
@@ -1,41 +1,78 @@
-function paiement(){
+// PAGE 2
+function paiement() {
     document.querySelector(".showAcheter").style.display = "none";
     document.querySelector(".footer").style.display = "none";
     document.querySelector(".onglet").innerHTML = "Paiement";
     document.querySelector(".showPaiement").style.display = "block";
+
+
+    let quantite = [];
+    let i = 0;
+    numbers.forEach(number => {
+        quantite.push([number.innerText, i])
+        i++
+        // console.log(quantite);
+    });
+    // Créez un objet pour stocker la quantité achetée pour chaque ticket
+    let quantitesParTicket = [];
+    quantite.forEach(number => {
+        ticketList.forEach(ticket => {
+            // On compare les deux id
+            if (ticket.id === number[1]) {
+                const qttAchetee = parseInt(number[0], 10);
+                // Vérifiez si la quantité est supérieure à zéro avant de l'ajouter à l'objet
+                if (qttAchetee > 0) {
+                    quantitesParTicket[ticket.id] = qttAchetee;
+                }
+                // console.log(qttAchetee);
+            }
+        });
+    });
+
+    // TEST POUR RECUPERER LE NOM, PRIX, ET QUANTITE EN FOREACH
+    let totContainer = document.querySelector(".facturation");
+    ticketList.forEach(ticket => {
+        if (quantitesParTicket.hasOwnProperty(ticket.id) && quantitesParTicket[ticket.id] > 0) {
+            let facturation = document.createElement("div")
+            facturation.classList.add('flex', 'payDiv')
+            let hr = document.createElement("hr")
+            facturation.innerHTML = `
+        <p class="nomTicket">`+ ticket.nom + `</p> <span class="prixTicket">` + ticket.prix / 100 + ` € x` + quantitesParTicket[ticket.id] + `</span>`
+            totContainer.appendChild(facturation);
+            totContainer.appendChild(hr);
+        }
+    })
+
+
+    /* // TEST POUR RECUPERER LE NOM, PRIX, ET QUANTITE EN FOREACH
+    let totContainer = document.querySelector(".facturation");
+    ticketList.forEach(ticket =>{
+        let facturation = document.createElement("div")
+        facturation.classList.add('flex', 'newdiv')
+        let hr = document.createElement("hr")
+        facturation.innerHTML = `
+        <p>`+ticket.nom+`</p> <span>`+ticket.prix /100+`</span>   <span> x `+quantitesParTicket[ticket.id]+` €</span>
+    
+        `
+        totContainer.appendChild(facturation);
+        totContainer.appendChild(hr);
+    }) */
+
 }
 
 function check() {
-    if(document.querySelector("#conditions").checked == true){
+    if (document.querySelector("#conditions").checked == true) {
         document.querySelector(".linkConditions").removeAttribute("disabled")
-     }
-   else if(document.querySelector("#conditions").checked == false){
+    }
+    else if (document.querySelector("#conditions").checked == false) {
         document.querySelector(".linkConditions").setAttribute("disabled", true)
     }
 }
 
-// TEST POUR RECUPERER LE NOM, PRIX, ET QUANTITE EN FOREACH
-/* let container = document.querySelector(".facturation");
-ticketList.forEach(ticket =>{
-    let facturation = document.createElement("div")
-    facturation.classList.add('flex', 'newdiv')
-    let hr = document.createElement("hr")
-    facturation.innerHTML = `
-    <p>`+ticket.nom+`</p> <span>`+ticket.prix / 100`</span>   <span>+" x " `` " €"</span>
-
-    `
-    container.appendChild(facturation);
-    container.appendChild(hr);
-})
- */
-
-
-
-
-
-
-function paiementCB(){
+function paiementCB() {
     document.querySelector(".showAcheter").style.display = "none";
     document.querySelector(".showPaiement").style.display = "none";
+    document.querySelector(".footer").style.display = "none";
     document.querySelector(".onglet").innerHTML = "Paiement";
+    document.querySelector(".showCB").style.display = "block";
 }
